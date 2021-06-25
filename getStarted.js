e14f0a23b7cfb4eeb2024c00a51e254ae4d2b349
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-25 23:09:44 +0530

    Add files via upload

---
+++ getStarted.js
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import './getStarted.css';
+
+
+class getStarted extends Component {
+  render() {
+  return (
+          
+    <div className="getStarted">
+    <div className="getStartedText">
+        <p>
+            Find the <span className="getStartedBest">best</span> suppliers and service 
+            <br/>
+            providers to get your business <span className="getStartedGrowing"> growing</span>
+        </p>
+    </div> 
+     <button type="button" className="getStartedButton">
+       <span className="getStartedButtonText"> Get Started</span>
+     </button>
+</div>
+           );
+}
+};
+
+export default getStarted;
